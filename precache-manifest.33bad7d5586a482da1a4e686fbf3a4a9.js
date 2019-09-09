e65c7f445941d1ce1f2e80e6941541558a446766
Roberto Ochoa <rochoa@intersysconsulting.com>
2019-09-09 15:07:07 -0500

    Updates

---
+++ precache-manifest.33bad7d5586a482da1a4e686fbf3a4a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b556858649f02a7df6e1b95b0eae32e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "98a99251fc2deac93fbf",
+    "url": "/monsters-rolodex/static/css/main.6bfd263d.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "98a99251fc2deac93fbf",
+    "url": "/monsters-rolodex/static/js/main.e1054b62.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
